570332780774ad1a19212fdabdd441163d97171c
2nner <khsbs001@naver.com>
2019-05-15 19:43:51 +0900

    Added Command & Modified Header File

--- socoban_project.c
+++ socoban_project.c
@@ -3,18 +3,19 @@
 #include <stdbool.h>
 #include <string.h>
 #include <termios.h>
-#include <unistd.h>
-#include <ctype.h>
 
 #define clear() printf("\033[H\033[J") // Cygwin에서 화면 깨끗이 해줌 - 사용법 : clear()
 
 int num_map; // 맵 개수
+int cnt_undo = 5; // 되돌리기 남은 횟수
+int current_stage = 1; // 현재 플레이중인 맵 스테이지
 char username[11]; // 유저 닉네임
-char PATH_MAP[] = "C:\\Users\\khsbs\\CLionProjects\\project\\map"; // map 파일이 있는 경로
+char PATH_MAP[] = "C:\\Users\\khsbs\\CLionProjects\\project\\map.txt"; // map 파일이 있는 경로
 char map[6][30][30]; // 맵 저장하는 곳
+char current_map[30][30]; // 현재 플레이중인 맵
 int length_garo[6], length_sero[6]; // Stage 별로 맵 가로 세로 길이 저장
 
-int getch(void) {
+int getch() {
     int ch;
     struct termios buf;
     struct termios save;
@@ -47,7 +48,7 @@ void checkMap() {
     if (f != NULL) {
         while (fscanf(f, "%s", str) != EOF) {
             if (strlen(str) == 1) {
-                if (isdigit(str[0]))
+                if ((str[0] - '0') >= 1 && (str[0] - '0') <= 5)
                     stage = str[0] - '0', pos = 0;
                 else break;
             }
@@ -79,9 +80,23 @@ void checkMap() {
     }
 }
 
+void printCommand() {
+    printf("h(왼족), j(아래), k(위), l(오른쪽)\n");
+    printf("u(undo) : 되돌리기 (남은 횟수 : %d번)\n", cnt_undo);
+    printf("r(replay) : 현재 맵 처음부터 다시 시작 (움직임 횟수 계속 유지됨)\n");
+    printf("n(new) : 첫 번째 맵부터 다시 시작 (움직임 횟수 기록은 삭제됨)\n");
+    printf("e(exit) : 게임 종료\n");
+    printf("s(save) : 세이브 파일에 저장\n");
+    printf("f(file load) : 세이브 파일 로드\n");
+    printf("d(display help) : 명령 내용 출력\n");
+    printf("t(top) : 게임 순위 출력, t 뒤에 숫자가 오면 해당 맵의 순위 출력\n");
+    getch();
+}
+
 // 닉네임 저장
 void getNickname() {
     while (1) {
+        char c;
         clear(); // Cygwin에서 화면 깔끔하게 지우고 프로그램 시작하기 위해서 사용
         printf("유저 이름 입력(영문으로 최대 10글자) : ");
         scanf("%s", username);
@@ -89,15 +104,30 @@ void getNickname() {
         if (strlen(username) <= 10)
             break;
         else
-            printf("최대 10글자까지 입력 가능합니다!");
+            printf("최대 10글자까지 입력 가능합니다!\n");
 
-        sleep(2);
+        getch();
+        getch();
     }
+}
+
+// 랭킹 출력
+void printRank() {
+
 }
 
 int main(void){
     checkMap();
     getNickname();
 
+    printCommand();
+    clear();
 
+    // 테스트 출력
+    for (int stage = 1; stage <= num_map; ++stage) {
+        printf("Stage %d\n", stage);
+        for (int i = 0; i < length_sero[stage]; ++i) {
+            printf("%s\n", map[stage][i]);
+        }
+    }
 }
