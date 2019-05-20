62b842cd4e4e1af66120b2d93211264203273e38
wjdghks0706 <wjdghks0706@naver.com>
2019-05-20 20:31:57 +0900

    움직임, 움직인 횟수, 스테이지클리어시 메시지, 스테이지 변경완료

--- socoban_project_vs.c
+++ socoban_project_vs.c
@@ -1,5 +1,6 @@
 #define _CRT_NONSTDC_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -12,10 +13,11 @@ int num_map; // 맵 개수
 int cnt_undo = 5; // 되돌리기 남은 횟수
 int current_stage = 1; // 현재 플레이중인 맵 스테이지
 char username[11]; // 유저 닉네임
-char PATH_MAP[] = "C:\\Users\\khsbs\\CLionProjects\\project\\map.txt"; // map 파일이 있는 경로
+char PATH_MAP[] = "C:\\Users\\wjdgh\\Desktop\\socoban_project\\map.txt"; // map 파일이 있는 경로
 char map[6][30][30]; // 맵 저장하는 곳
 char current_map[30][30]; // 현재 플레이중인 맵
 int length_garo[6], length_sero[6]; // Stage 별로 맵 가로 세로 길이 저장
+int count = 0; // 이동한 횟수 저장
 
 // 박스 개수와 보관 장소 개수가 다를 때 출력하는 에러 메세지. 출력 후 프로그램이 종료됨
 void error(int i) {
@@ -66,6 +68,7 @@ void checkMap() {
 	}
 }
 
+// 커맨드 출력
 void printCommand() {
 	printf("h(왼족), j(아래), k(위), l(오른쪽)\n");
 	printf("u(undo) : 되돌리기 (남은 횟수 : %d번)\n", cnt_undo);
@@ -76,13 +79,11 @@ void printCommand() {
 	printf("f(file load) : 세이브 파일 로드\n");
 	printf("d(display help) : 명령 내용 출력\n");
 	printf("t(top) : 게임 순위 출력, t 뒤에 숫자가 오면 해당 맵의 순위 출력\n");
-	_getch();
 }
 
 // 닉네임 저장
 void getNickname() {
 	while (1) {
-		char c;
 		clear(); // Cygwin에서 화면 깔끔하게 지우고 프로그램 시작하기 위해서 사용
 		printf("유저 이름 입력(영문으로 최대 10글자) : ");
 		scanf("%s", username);
@@ -92,8 +93,8 @@ void getNickname() {
 		else
 			printf("최대 10글자까지 입력 가능합니다!\n");
 
-		_getch();
-		_getch();
+		getch();
+		getch();
 	}
 }
 
@@ -106,14 +107,222 @@ int main(void) {
 	checkMap();
 	getNickname();
 
-	printCommand();
 	clear();
 
-	// 테스트 출력
+	// 게임 시작
 	for (int stage = 1; stage <= num_map; ++stage) {
-		printf("Stage %d\n", stage);
-		for (int i = 0; i < length_sero[stage]; ++i) {
-			printf("%s\n", map[stage][i]);
+		// 스테이지 복사
+		for (int i = 0; i < length_sero[current_stage]; ++i) {
+			strcpy(current_map[i], map[current_stage][i]);
+		}
+
+		int pwd_g, pwd_s; // pwd_g : 가로 현재 위치, pwd_s : 세로 현재 위치
+
+		// 현재 위치 탐색
+		for (int i = 0; i < length_sero[current_stage]; ++i) {
+			int flag = 0;
+			for (int j = 0; j < length_garo[current_stage]; ++j) {
+				if (current_map[i][j] == '@') {
+					pwd_s = i, pwd_g = j, flag = 1;
+					break;
+				}
+			}
+
+			if (flag) break;
+		}
+
+		while (1) {
+			system("cls");
+			char c;
+			int end_count = 0; // 구멍개수 세는 변수
+
+
+			// 맵 출력
+			for (int i = 0; i < length_sero[current_stage]; ++i) {
+				printf("%s\n", current_map[i]);
+
+			}
+			printf("%d", count);
+
+			c = getch();
+			switch (c) {
+			case 75: { // h
+				if (current_map[pwd_s][pwd_g - 1] == '.' || current_map[pwd_s][pwd_g - 1] == 'O') {
+					if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+						current_map[pwd_s][pwd_g] = '.';
+					}
+					else {
+						current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+					}
+				}
+				else if (current_map[pwd_s][pwd_g - 1] == '#') continue;
+				else if (current_map[pwd_s][pwd_g - 1] == '$') {
+					if (current_map[pwd_s][pwd_g - 2] == '$' || current_map[pwd_s][pwd_g - 2] == '#') continue;
+					if (current_map[pwd_s][pwd_g - 2] == '.') {
+						current_map[pwd_s][pwd_g - 2] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+					else if (current_map[pwd_s][pwd_g - 2] == 'O') {
+						current_map[pwd_s][pwd_g - 2] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+				}
+
+				current_map[pwd_s][pwd_g - 1] = '@';
+				pwd_g -= 1;
+				count++;
+				break;
+			}
+
+			case 80: { // j
+				if (current_map[pwd_s + 1][pwd_g] == '.' || current_map[pwd_s + 1][pwd_g] == 'O') {
+					if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+						current_map[pwd_s][pwd_g] = '.';
+					}
+					else {
+						current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+					}
+				}
+				else if (current_map[pwd_s + 1][pwd_g] == '#') continue;
+				else if (current_map[pwd_s + 1][pwd_g] == '$') {
+					if (current_map[pwd_s + 2][pwd_g] == '$' || current_map[pwd_s + 2][pwd_g] == '#') continue;
+					if (current_map[pwd_s + 2][pwd_g] == '.') {
+						current_map[pwd_s + 2][pwd_g] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+					else if (current_map[pwd_s + 2][pwd_g] == 'O') {
+						current_map[pwd_s + 2][pwd_g] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+				}
+
+				current_map[pwd_s + 1][pwd_g] = '@';
+				pwd_s += 1;
+				count++;
+				break;
+			}
+
+			case 72: { // k
+				if (current_map[pwd_s - 1][pwd_g] == '.' || current_map[pwd_s - 1][pwd_g] == 'O') {
+					if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+						current_map[pwd_s][pwd_g] = '.';
+					}
+					else {
+						current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+					}
+				}
+				else if (current_map[pwd_s - 1][pwd_g] == '#') continue;
+				else if (current_map[pwd_s - 1][pwd_g] == '$') {
+					if (current_map[pwd_s - 2][pwd_g] == '$' || current_map[pwd_s - 2][pwd_g] == '#') continue;
+					if (current_map[pwd_s - 2][pwd_g] == '.') {
+						current_map[pwd_s - 2][pwd_g] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+					else if (current_map[pwd_s - 2][pwd_g] == 'O') {
+						current_map[pwd_s - 2][pwd_g] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+				}
+
+				current_map[pwd_s - 1][pwd_g] = '@';
+				pwd_s -= 1;
+				count++;
+				break;
+			}
+
+			case 77: { // l
+				if (current_map[pwd_s][pwd_g + 1] == '.' || current_map[pwd_s][pwd_g + 1] == 'O') {
+					if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+						current_map[pwd_s][pwd_g] = '.';
+					}
+					else {
+						current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+					}
+				}
+				else if (current_map[pwd_s][pwd_g + 1] == '#') continue;
+				else if (current_map[pwd_s][pwd_g + 1] == '$') {
+					if (current_map[pwd_s][pwd_g + 2] == '$' || current_map[pwd_s][pwd_g + 2] == '#') continue;
+					if (current_map[pwd_s][pwd_g + 2] == '.') {
+						current_map[pwd_s][pwd_g + 2] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+					else if (current_map[pwd_s][pwd_g + 2] == 'O') {
+						current_map[pwd_s][pwd_g + 2] = '$';
+						if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
+							current_map[pwd_s][pwd_g] = '.';
+						}
+						else {
+							current_map[pwd_s][pwd_g] = map[current_stage][pwd_s][pwd_g];
+						}
+					}
+				}
+
+				current_map[pwd_s][pwd_g + 1] = '@';
+				pwd_g += 1;
+				count++;
+				break;
+			}
+
+
+			}
+			for (int s = 0; s < length_sero[1]; s++) {
+
+				for (int g = 0; g < length_garo[1]; g++)
+				{
+					if (current_map[s][g] == '0')
+					{
+						end_count++;
+					}
+
+
+
+				}
+			}
+
+
+
+
+
+			if (end_count = 0 && map[1][pwd_s][pwd_g] != '0')
+				break;
+
+
 		}
 	}
+	printf("스테이지를 모두 완료하셨네요 축하합니다. \n");
 }
