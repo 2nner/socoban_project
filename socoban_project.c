d9a6e8f6642929868f96e21f61acaee8c1274fe3
2nner <khsbs001@naver.com>
2019-05-28 17:11:51 +0900

    기능 병합 및 파일 삭제

--- socoban_project.c
+++ socoban_project.c
@@ -1,21 +1,27 @@
+#define _CRT_NONSTDC_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <termios.h>
-
-#define clear() printf("\033[H\033[J") // Cygwin에서 화면 깨끗이 해줌 - 사용법 : clear()
+#include <conio.h> // Cygwin 사용 시 주석 처리
+//#include <termios.h> // Cygwin 사용 시 주석 해제
 
 int num_map; // 맵 개수
 int cnt_undo = 5; // 되돌리기 남은 횟수
 char username[11]; // 유저 닉네임
-char PATH_MAP[] = "C:\\Users\\khsbs\\CLionProjects\\project\\map.txt"; // map 파일이 있는 경로
+char PATH_MAP[] = ""; // map 파일이 있는 경로
 char map[6][30][30]; // 맵 저장하는 곳
 char current_map[30][30]; // 현재 플레이중인 맵
 int length_garo[6], length_sero[6]; // Stage 별로 맵 가로 세로 길이 저장
 int pwd_g, pwd_s; // pwd_g : 가로 현재 위치, pwd_s : 세로 현재 위치
+int current_stage; // 현재 플레이중인 스테이지
 int count = 0; // 이동한 횟수 저장
+int end_count; // 남은 구멍 개수
 
+int before_map[6][30][30]; // 백업용 맵
+
+/* Cygwin 사용 시 주석 해제
 int getch() {
 	int ch;
 	struct termios buf;
@@ -30,6 +36,18 @@ int getch() {
 	ch = getchar();
 	tcsetattr(0, TCSAFLUSH, &save);
 	return ch;
+}
+*/
+
+// 화면 깨끗하게 해주는 함수, Cygwin 사용 시 cls를 clear로 바꿀 것
+void clear() {
+	system("cls");
+}
+
+// 박스 개수와 보관 장소 개수가 다를 때 출력하는 에러 메세지. 출력 후 프로그램이 종료됨
+void error(int i) {
+	printf("Stage %d의 박스 개수와 보관장소 개수가 같지 않거나, 옮길 박스 혹은 보관 장소가 존재하지 않습니다. 프로그램을 종료합니다.", i);
+	exit(EXIT_FAILURE);
 }
 
 // 맵 분리 & 스테이지 개수 카운트 & 스테이지 별로 박스($) 개수와 보관 장소(O) 개수가 같지 않거나 아예 없을 경우 에러 출력 후 프로그램 종료
@@ -107,6 +125,18 @@ void getNickname() {
 
 // 움직임이 구현되있는 함수, 사용법 : move(울직이는 세로 칸 수, 움직이는 가로 칸 수, 현재 스테이지 넘버)
 void move(int s, int g, int current_stage) {
+
+	//undo를 쓰기위한 이전 맵 데이터 저장 (움직일 때마다 백업맵에다 저장)
+	for (int i = 4; i >= 0; i--) {
+		for (int j = 0; j < length_sero[current_stage]; ++j) {
+			strcpy(before_map[i + 1][j], before_map[i][j]);
+		}
+	}
+	for (int i = 0; i < length_sero[current_stage]; ++i) {
+		strcpy(before_map[0][i], current_map[i]);
+	}
+
+	//움직임 구현
 	if (current_map[pwd_s + s][pwd_g + g] == '.' || current_map[pwd_s + s][pwd_g + g] == 'O') {
 		if (map[current_stage][pwd_s][pwd_g] == '@' || map[current_stage][pwd_s][pwd_g] == '$') {
 			current_map[pwd_s][pwd_g] = '.';
@@ -141,6 +171,7 @@ void move(int s, int g, int current_stage) {
 	current_map[pwd_s + s][pwd_g + g] = '@';
 	pwd_s += s;
 	pwd_g += g;
+
 }
 
 // 랭킹 출력
@@ -148,21 +179,133 @@ void printRank() {
 
 }
 
+// 저장
+void save()
+{
+	FILE* save;
+	save = fopen("sokoban.txt", "w");
+	// fprintf(save, "유저 이름 : %s\n", username);
+	fprintf(save, "Stage %d\n", current_stage);
+	for (int i = 0; i < length_sero[current_stage]; ++i) {
+		fprintf(save, "%s\n", current_map[i]);
+	}
+	fprintf(save, "움직인 횟수 : %d\n", count);
+	fprintf(save, "남은 구멍 개수 : %d\n", end_count);
+	fclose(save);
+}
+
+// 현재 맵 처음부터 다시 시작
+void replay() {
+
+
+	clear();
+
+
+	for (int i = 0; i < length_sero[current_stage]; ++i) {
+		strcpy(current_map[i], map[current_stage][i]);
+	}
+
+
+	for (int i = 0; i < length_sero[current_stage]; ++i) {
+		int flag = 0;
+		for (int j = 0; j < length_garo[current_stage]; ++j) {
+			if (current_map[i][j] == '@') {
+				pwd_s = i, pwd_g = j, flag = 1;
+				break;
+			}
+		}
+
+		if (flag) break;
+
+	}
+
+
+}
+
+// u키를 누르면 이전으로 돌아감 (undo 구현, 현재맵을 백업파일로 바꾸고 횟수 1 차감함)
+void undo() {
+	if (cnt_undo != 0) {
+		for (int i = 0; i < length_sero[current_stage]; ++i) {
+			strcpy(current_map[i], before_map[0][i]);
+		}
+		for (int i = 0; i <= 4; i++) {
+			for (int j = 0; j < length_sero[current_stage]; ++j) {
+				strcpy(before_map[i][j], before_map[i + 1][j]);
+			}
+		}
+		for (int i = 0; i < length_sero[current_stage]; ++i) {
+			int flag = 0;
+			for (int j = 0; j < length_garo[current_stage]; ++j) {
+				if (current_map[i][j] == '@') {
+					pwd_s = i, pwd_g = j, flag = 1;
+					break;
+				}
+			}
+
+			if (flag) break;
+
+		}
+		cnt_undo -= 1;
+	}
+}
+
+// e키를 누르면 종료함
+void quit() {
+	exit(0);
+}
+
+// n키를 누르면 1라운드부터 재시작함
+void newstart() {
+	clear();
+
+	current_stage = 1;
+	count = 0;
+	cnt_undo = 5;
+
+	for (int i = 0; i < length_sero[current_stage]; ++i) {
+		strcpy(current_map[i], map[current_stage][i]);
+	}
+
+
+	for (int i = 0; i < length_sero[current_stage]; ++i) {
+		int flag = 0;
+		for (int j = 0; j < length_garo[current_stage]; ++j) {
+			if (current_map[i][j] == '@') {
+				pwd_s = i, pwd_g = j, flag = 1;
+				break;
+			}
+		}
+
+		if (flag) break;
+
+	}
+
+
+}
 
 int main(void) {
 	checkMap();
 	getNickname();
 
-	printCommand();
 	clear();
 
 	// 게임 시작
-	for (int current_stage = 1; current_stage <= num_map; ++current_stage) {
+	for (current_stage = 1; current_stage <= num_map; ++current_stage) {
+
 		// 스테이지 복사
 		for (int i = 0; i < length_sero[current_stage]; ++i) {
 			strcpy(current_map[i], map[current_stage][i]);
 		}
 
+		// 초기 undo를 위한 맵 백업(현재맵으로 모두초기화) + cnt_undo = 5 초기화
+		cnt_undo = 5;
+		for (int i = 0; i <= 5; i++) {
+			for (int j = 0; j < length_sero[current_stage]; ++j) {
+				strcpy(before_map[i][j], current_map[j]);
+			}
+		}
+
+		// 움직인 횟수 초기화
 		count = 0;
 
 		// 현재 위치 탐색
@@ -189,6 +332,7 @@ int main(void) {
 				printf("%s\n", current_map[i]);
 			}
 			printf("움직인 횟수 : %d\n", count);
+			printf("남은 undo 횟수 : %d\n", cnt_undo);
 
 			// 지정된 장소 안에 박스가 다 들어갔는 지 체크하고, 참일 시에 while문 탈출
 			for (int s = 0; s < length_sero[1]; s++) {
@@ -203,27 +347,55 @@ int main(void) {
 
 			c = getch();
 			switch (c) {
-			case 'h': { // 75
+			case 75: { // h
 				count++;
 				move(0, -1, current_stage);
 				break;
 			}
 
-			case 'j': { // 80
+			case 80: { // j
 				count++;
 				move(1, 0, current_stage);
 				break;
 			}
 
-			case 'k': { // 72
+			case 72: { // k
 				count++;
 				move(-1, 0, current_stage);
 				break;
 			}
 
-			case 'l': { // 77
+			case 77: { // l
 				count++;
 				move(0, 1, current_stage);
+				break;
+			}
+
+			case 115: { // s
+				save();
+				printf("\n    저장이 완료되었습니다\n(계속하려면 아무키나 누르세요)");
+				getch();
+				break;
+			}
+
+			case 114: { // r
+				replay();
+				break;
+			}
+			case 'e': { //e
+				save();
+				printf("\n    저장이 완료되었습니다. 종료합니다.\n");
+				quit();
+				break;
+			}
+
+			case 'n': { //n
+				newstart();
+				break;
+			}
+
+			case 'u': { //u
+				undo();
 				break;
 			}
 			}
